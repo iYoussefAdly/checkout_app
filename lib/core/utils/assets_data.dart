43f4e9c12ebe84bd7c290707af4e19781f6c45b6
Youssef <youssefadly332@gmail.com>
2026-04-01 20:17:45 +0200

    finish PaymentDetailsView

--- lib/core/utils/assets_data.dart
+++ lib/core/utils/assets_data.dart
@@ -1,3 +1,6 @@
 abstract class AssetsData {
   static const basketImage = "assets/images/Group 6.png";
+  static const cardImage = "assets/images/card_image.svg";
+  static const paypalImage = "assets/images/paypal_image.svg";
+  static const payImage = "assets/images/pay_image.svg";
 }
